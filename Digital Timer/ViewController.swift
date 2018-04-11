9c4cb7ff564d3a5161f7eba3b0a0db18d6861d21
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-04-11 12:39:30 +0900

    add switch

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var timerStrat: UISwitch!
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        timerStrat.isOn = false
+        
+        //클로저 Closure의 사용
+//        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
+//            (myTimer) in
+//            self.updateTime()
+//
+//
+//        })
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func updateTime () {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss a"
+        lblTime.text = formatter.string(from: date)
+    }
+    @IBAction func switchChanged(_ sender: Any) {
+        if timerStrat.isOn == true {
+            // closure 사용
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()
+            })
+            
+        } else {
+            myTimer.invalidate()
+            
+        }
+    }
+    
 
 }
 
